cd6d13e88cbffee4c7b43e4ae5b5ae76a36d954a
Daniel Prieto <88695568+danobanano88@users.noreply.github.com>
2022-05-03 16:48:37 -0500

    cambios a comentarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {
   animate,
   group,
   animateChild
-} from '@angular/animations';
+} from '@angular/animations';  //& imports necesarios para las animaciones 
 
 
 @Component({
@@ -15,17 +15,17 @@ import {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
-    trigger('myAnimation', [  //declaracion del div en el que se va a actiar la animacion, rodea al router outlet
-      transition('* <=> *', [ //Transicion en la que se va a activar "*<=>*"
+    trigger('myAnimation', [  //& declaracion del div en el que se va a actiar la animacion, rodea al router outlet
+      transition('* <=> *', [ //& Transicion en la que se va a activar "*<=>*"
         query(
           ':enter, :leave',
           [style({ position: 'fixed', width:'100%'  })],
           { optional: true }
         ),
-       group([query( //grupo de animaciones que suceden 
-          ':leave', //se activa cuando...
-           [style({ transform: 'translateX(0%)'}), 
-            animate('0.5s ease-in-out', //tipo de animacion y duracion 
+       group([query( //& grupo de animaciones que suceden 
+          ':leave', //& se activa cuando...
+           [style({ transform: 'translateX(0%)'}), //& decide que va a pasar con lo que estaba previamente en la pagina, en este caso lo desaparece
+            animate('0.5s ease-in-out', //& tipo de animacion y duracion 
             style({ transform: 'translateX(-100%)' }))],
           { optional: true }
         ),
@@ -33,13 +33,14 @@ import {
           ':enter',
           [style({ transform: 'translateX(100%)' }),
              animate('0.5s ease-in-out', 
-             style({ transform: 'translateX(0%)' }))],
+             style({ transform: 'translateX(0%)' }))
+            ],
           { optional: true }
         )]) 
       ])
     ]),
     
-      ] // register the animations
+      ] //& Todo el apartado de animaciones
     
     })
     export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; //& Modulo de animaciones 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
 import { HomeComponent } from './home/home.component';
+import { OpenCloseComponent } from './open-close/open-close.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
     DetailComponent,
-    HomeComponent
+    HomeComponent,
+    OpenCloseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule   
+    BrowserAnimationsModule   //& Importacion de las animaciones
   ],
   providers: [],
   bootstrap: [AppComponent]
