f76278833e4fbc0af3ea57c2f34da24a5514c261
Daniel Prieto <88695568+danobanano88@users.noreply.github.com>
2022-05-03 15:20:10 -0500

    Animaciones angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import {
+  transition,
+  trigger,
+  query,
+  style,
+  animate,
+  group,
+  animateChild
+} from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'transitionsP';
-}
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myAnimation', [  //declaracion del div en el que se va a actiar la animacion, rodea al router outlet
+      transition('* <=> *', [ //Transicion en la que se va a activar "*<=>*"
+        query(
+          ':enter, :leave',
+          [style({ position: 'fixed', width:'100%'  })],
+          { optional: true }
+        ),
+       group([query( //grupo de animaciones que suceden 
+          ':leave', //se activa cuando...
+           [style({ transform: 'translateX(0%)'}), 
+            animate('0.5s ease-in-out', //tipo de animacion y duracion 
+            style({ transform: 'translateX(-100%)' }))],
+          { optional: true }
+        ),
+        query(
+          ':enter',
+          [style({ transform: 'translateX(100%)' }),
+             animate('0.5s ease-in-out', 
+             style({ transform: 'translateX(0%)' }))],
+          { optional: true }
+        )]) 
+      ])
+    ]),
+    
+      ] // register the animations
+    
+    })
+    export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
